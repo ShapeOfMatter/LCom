db1a7a90a2a54ecb8cfd915b4dde2c3afe51a9b2
Mako Bates <33739499+ShapeOfMatter@users.noreply.github.com>
2021-12-09 12:26:48 -0500

    runParty is ready

--- LCom/src/Communicate.hs
+++ LCom/src/Communicate.hs
@@ -4,27 +4,32 @@ module Communicate
     , locally
     , send
     , runClique
-    --, ignoreHead
+    , runParty
     ) where
 
 import Data.Distributive (distribute)
 import Data.Type.Set (Subset)
 import Data.Vec.Lazy (Vec)
-import Polysemy (interpret, Member, reinterpret, Sem)
+import Polysemy (interpret, Member, reinterpret, reinterpret2, Sem, subsume_)
+import Polysemy.Input (Input, input)
 import qualified Polysemy.Internal as PI  -- God help us.
+import Polysemy.Output (Output, output)
 
-import Data (deserialize, Located(Located), Party, Sendable, serialize)
+import Data (Address, address, Addresses, addresses, deserialize, Located(Located), Party, Sendable, serialize)
+
 
 
 -------- Effect Signatures --------
 
 data Communicate (parties :: [Party]) s m a where  -- s is for subject, as in the subject of the verb "communicate".
   SendMaybe :: forall (recipients :: [Party]) (senders :: [Party]) parties s m.
+               (Addresses recipients, Addresses senders) =>
                Located senders (Maybe s) -> Communicate parties s m (Located recipients (Maybe s))
   Locally :: Located parties a -> Communicate parties s m a
 
 sendMaybe :: forall recipients senders parties s r.
-             (Member (Communicate parties s) r) =>
+             (Addresses recipients, Addresses senders,
+              Member (Communicate parties s) r) =>
              Located senders (Maybe s) -> Sem r (Located recipients (Maybe s))
 sendMaybe x = PI.send $ SendMaybe @recipients x
 locally :: (Member (Communicate parties s) r) => Located parties a  -> Sem r a
@@ -48,6 +53,8 @@ runClique = (Located <$>) . (reinterpret _clique)
 send :: forall (recipients :: [Party]) n (senders :: [Party]) (parties :: [Party]) r s t .
         (Subset recipients parties,
          Subset senders parties,
+         Addresses recipients,
+         Addresses senders,
          Member (Communicate parties s) r,
          Sendable s t n) =>
         Located senders t -> Sem r (Located recipients t)
@@ -66,86 +73,64 @@ noEffectSingleThread = interpret $ \case
   SendMaybe (Located v) -> return (Located v)
   Locally (Located v) -> return v
 
-{-
 -- Requisite for all the other handlers:
 data Transmit (parties :: [Party]) s m a where
   TransmitMaybe :: forall senders parties s m.
-                   SubsetProof senders parties
-                   -> Located senders (Maybe s)
+                   (Addresses senders) =>
+                   Located senders (Maybe s)
                    -> Transmit parties s m ()
 transmitMaybe :: forall senders parties s r.
-                 (Member (Transmit parties s) r) =>
-                 SubsetProof senders parties
-                 -> Located senders (Maybe s)
+                 (Addresses senders,
+                  Member (Transmit parties s) r) =>
+                 Located senders (Maybe s)
                  -> Sem r ()
-transmitMaybe sp x = PI.send $ TransmitMaybe sp x
+transmitMaybe x = PI.send $ TransmitMaybe x
+
+runTransmit :: forall p parties s r a.
+               (Address p) =>
+               Sem (Transmit parties s ': r) a -> Sem (Output ([Integer], Maybe s) ': r) a
+runTransmit = reinterpret $ \case
+  TransmitMaybe l -> rt l
+  where rt :: forall senders.
+              (Addresses senders) =>
+              Located senders (Maybe s) -> Sem (Output ([Integer], Maybe s) ': r) ()
+        rt (Located ms) = if address @p `elem` addresses @senders then output (addresses @senders, ms) else return ()
+
 data Recieve (parties :: [Party]) s m a where
   RecieveMaybe :: forall recipients parties s m.
-                  SubsetProof recipients parties
-                  -> Recieve parties s m (Located recipients (Maybe s))
+                  (Addresses recipients) =>
+                  Recieve parties s m (Located recipients (Maybe s))
 recieveMaybe :: forall recipients parties s r.
-                (Member (Recieve parties s) r) =>
-                SubsetProof recipients parties
-                -> Sem r (Located recipients (Maybe s))
-recieveMaybe sp = PI.send $ RecieveMaybe sp
+                (Addresses recipients,
+                 Member (Recieve parties s) r) =>
+                Sem r (Located recipients (Maybe s))
+recieveMaybe = PI.send $ RecieveMaybe
+
+runRecieve :: forall p parties s r a.
+              (Address p) =>
+              Sem (Recieve parties s ': r) a -> Sem (Input (Maybe s) ': r) a
+runRecieve = reinterpret $ \case
+  rm@(RecieveMaybe) -> rr rm
+  where rr :: forall recipients m.
+              (Addresses recipients) =>
+              Recieve parties s m (Located recipients (Maybe s)) -> Sem (Input (Maybe s) ': r) (Located recipients (Maybe s))
+        rr = if address @p `elem` addresses @recipients then const (Located <$> input) else const (return $ Located undefined)
 
 splitTasks :: forall parties s r a.
               Sem (Communicate parties s ': r) a -> Sem (Transmit parties s ': Recieve parties s ': r) a
 splitTasks = reinterpret2 $ \case -- possibly a layer of State here would help with the dispatching?
-  SendMaybe rp sp x -> do transmitMaybe sp x
-                          recieveMaybe rp
+  SendMaybe x -> do transmitMaybe x
+                    recieveMaybe
   Locally (Located v) -> return v
-
-
-class IgnoreSend (p :: Party) (recipients :: [Party]) (senders :: [Party]) (parties :: [Party]) where
-  ignoreSend :: (Member (Communicate parties s) r) =>
-                SubsetProof recipients (p ': parties)
-                -> SubsetProof senders (p ': parties)
-                -> Located senders (Maybe s)
-                -> Sem r (Located recipients (Maybe s))
---instance p recipients senders '[] where
---  ignoreSend _ _ _ = return pretend
-instance IgnoreSend p (p ': recipients) (p ': senders) (p ': parties) where
-  ignoreSend rp sp (Located m) = do (Located m') <- sendMaybe (subsetTail rp) (subsetTail sp) (Located m)
-                                    return (Located m')
- 
-
-{-
-ignoreHead :: forall p parties s r a.
-              (IsSet (p ': parties)) =>
-              Sem (Communicate (p ': parties) s ': r) a -> Sem (Communicate parties s ': r) a
-ignoreHead = reinterpret $ \case  -- I think I can make this work using a closed type family or something; IDK.
-  SendMaybe rp sp (Located v) -> ignoreSend rp sp (Located v)
-  Locally (Located v) -> undefined
-
-class MakeSend (p :: Party) (recipients :: [Party]) (senders :: [Party]) (parties :: [Party]) where
-  makeSend :: (Members '[Input (Maybe s), Output ([Integer], Maybe s), Communicate parties s] r) =>
-              SubsetProof recipients (p ': parties)
-              -> SubsetProof senders (p ': parties)
-              -> Located senders v
-              -> Sem r (Located recipients v)
---instance MakeSend p recipients senders '[] where
---  makeSend _ _ _ = return pretend
-instance MakeSend p recipients '[] (p ': parties) where
-  makeSend _ _ _ = return pretend
-instance MakeSend p '[] senders parties where
-  makeSend _ _ _ = return pretend
-instance MakeSend p (p ': recipients) (p ': senders) (p ': parties) where
-  makeSend _ _ (Located v) = return (Located v)
-instance MakeSend p (p ': recipients) senders (p ': parties) where
-  makeSend _ _ (Located v) = undefined
 
 -- Communication by the local party would turn into IO read/write on a network port;
 -- all other communication should simply be skipped.
-runHead :: forall p parties s r a.
-           (IsSet (p ': parties)) =>
-           Sem (Communicate (p ': parties) s ': r) a -> Sem (Communicate parties s ': Input (Maybe s) ': Output ([Integer], Maybe s) ': r) a
-runHead = reinterpret3 $ \case  -- I think I can make this work using a closed type family or something; IDK.
-  SendMaybe rp sp (Located v) -> undefined
-  Locally (Located v) -> undefined
--}
+runParty :: forall p parties s r a.
+            (Address p) =>
+            Sem (Communicate parties s ': r) a -> Sem (Input (Maybe s) ': Output ([Integer], Maybe s) ': r) a
+runParty = runRecieve @p . subsume_ . runTransmit @p . splitTasks
 
 -- And there should be another handler, similar to noEffectSingleThread,
 -- which will run single-threaded by collect a structured log of all communication. 
--}
+--}
 
